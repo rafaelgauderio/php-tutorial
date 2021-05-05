e8b0f0e39283db415ebf7361e1ef661573bb3e88
Rafael De Luca <deluca1712@gmail.com>
2021-05-05 12:16:54 -0300

    ajuste exercicio_natacao

--- introducao/exercicio_natacao.php
+++ introducao/exercicio_natacao.php
@@ -10,42 +10,49 @@ $categorias[] = 'idoso';
 
 print_r($categorias);
 
+
+echo("<br/>\n");
+
 $nome = 'Rafael De Luca';
-$idade = 29;
+$idade = 35;
+
 
 var_dump($nome);
 var_dump($idade);
+
+echo("<br/>\n");
 
 if ($idade<0) {
 	echo 'Tu informaste uma idade inválida';
 } 
 
-if else ($idade >=0 && $idade< 6) {
-	for ($i =0; $i<=count($categorias) ; i++) {
-		if($categorias[$i] == 'fraldinha') {
-			echo "O nadador ",$nome, " compete na categoria fraldinha";
-		}
+else if ($idade >=0 && $idade< 6) {
+	for ($i =0; $i<=count($categorias); $i++) {
+		if($categorias[$i] == 'fraldinha') 
+			echo "O nadador ".$nome. " compete na categoria fraldinha";
+		
 	}
 }
 
-else if($idade>=6 && idade <=12) {
-	for($i=0; &i<=count($categorias) ; i++) {
-		if($categorias[$i] == 'infantil') {
-			echo "O nadador ",$nome, " compete na categoria infantil";
-		}
+else if($idade>=6 && $idade <=12) {
+	for($i=0; $i<=count($categorias); $i++) {
+		if($categorias[$i] == 'infantil') 
+			echo "O nadador ".$nome. " compete na categoria infantil";
+		
 	}	
 
 }
- 
-else if(&idade>12 && idade <=18) {
-	for($i=0; &i<=count($categorias) ; i++) {
-		if($categorias[$i] == 'adolescente') {
-			echo "O nadador ",$nome, " compete na categoria " .$categorias[$i];
-		}
+
+else if($idade>12 && $idade <=18) {
+	for($i=0; $i<=count($categorias) ; $i++) {
+		if($categorias[$i] == 'adolescente') 
+			echo "O nadador ".$nome. " compete na categoria " .$categorias[$i];
+		
 	}	
-}
+} 
+
 
 else {
-	echo 'adulto';
-
+	echo 'O nadador é adulto';
+		}
 ?>
