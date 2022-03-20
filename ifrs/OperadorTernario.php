a56568dd5a64cbd8509d9a3e1af298dd981958e4
Rafael De Luca <deluca1712@gmail.com>
2022-03-20 17:32:20 -0300

    Operador ternario

---
+++ ifrs/OperadorTernario.php
@@ -0,0 +1,21 @@
+<?php
+
+//Operador ternário
+
+<?php
+
+//Operador ternário
+
+$x=30;
+echo "<br>" ;
+echo $x==10 ? "Valor igual a 10" : "Valor diferente de 10";
+
+$idade=15;
+echo "<br>";
+echo $idade==15 ? "15 anos de idade" : "Idade diferente de 15";
+
+$idade=25;
+echo "<br>";
+echo $idade==15 ? "15 anos de idade" : "Idade diferente de 15";
+
+?>
