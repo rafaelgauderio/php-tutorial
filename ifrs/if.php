67fea13156bec3284edfb26e6335c6e0a51b7b91
Rafael De Luca <deluca1712@gmail.com>
2022-03-20 16:37:47 -0300

    if com php

---
+++ ifrs/if.php
@@ -0,0 +1,64 @@
+<?php
+
+$x=30;
+$y=45;
+echo "Valor de x: $x";
+echo "<br>Valor de y: $y";
+
+if($x > 15 && $y > 15) {
+	echo "<br>Ambos valores informados são <b>maiores</b> que 15!";	
+}
+else {
+
+	echo "<br>Pelo menos um dos valores informados é <b>menor</b> que 15!";	
+}
+
+if($x > 15 || $y > 15) {
+	echo "<br>Pelo menos um dos valores é <b>maior</b> que 15!";	
+}
+else {
+
+	echo "<br>Ambos os valores informados são <b>menores</b> que 15!";	;	
+}
+
+$x=7;
+$y=8;
+echo "<br>Valor de x: $x";
+echo "<br>Valor de y: $y";
+
+if($x > 15 && $y > 15) {
+	echo "<br>Ambos valores informados são <b>maiores</b> que 15!";	
+}
+else {
+
+	echo "<br>Pelo menos um dos valores informados é <b>menor</b> que 15!";	
+}
+
+if($x > 15 || $y > 15) {
+	echo "<br>Pelo menos um dos valores é <b>maior</b> que 15!";	
+}
+else {
+
+	echo "<br>Ambos os valores informados são <b>menores</b> que 15!";	
+}
+
+$x=18;
+$y=8;
+echo "<br>Valor de x: $x";
+echo "<br>Valor de y: $y";
+
+if($x > 15 and $y > 15) {
+	echo "<br>Ambos valores informados são <b>maiores</b> que 15!";	
+}
+else {
+
+	echo "<br>Pelo menos um dos valores informados é <b>menor</b> que 15!";	
+}
+
+if($x > 15 or $y > 15) {
+	echo "<br>Pelo menos um dos valores é <b>maior</b> que 15!";	
+}
+else {
+
+	echo "<br>Ambos os valores informados são <b>menores</b> que 15!";	
+}
