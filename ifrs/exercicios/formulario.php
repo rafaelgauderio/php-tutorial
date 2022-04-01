06d386aba846d7891455c6a5ae47c03bfb7df2e2
Rafael De Luca <deluca1712@gmail.com>
2022-04-01 00:32:28 -0300

    Correção exercicio formulario

--- ifrs/exercicios/formulario.php
+++ ifrs/exercicios/formulario.php
@@ -1,19 +1,24 @@
 <?php
 
 if($_REQUEST['nome'] == '' || $_REQUEST['email'] == '') {
-  header('Location: formulario.php');	
+  header('Location: cadastro.html');	
 }
 
 
 
 $nome=filter_var($_REQUEST['nome'],FILTER_SANITIZE_STRING);
-echo $nome . "<br>";
+echo "Nome: " . $nome . "<br>";
 
 $email=@filter_var($_REQUEST['email'],FILTER_SANITIZE_STRING,FILTER_VALIDATE_EMAIL);
-echo $email . "<br>";
+if(FILTER_VALIDATE_EMAIL==true) {
+	echo "Email: " . $email . "<br>";
+} else {
+	header('Location: cadastro.html');
+}
+
 
 $sexo=@filter_var($_REQUEST['sexo'],FILTER_SANITIZE_STRING);
-echo $sexo . "<br>";
+echo "Sexo: " . $sexo . "<br>";
 
 $datanasc=@filter_var($_REQUEST['datanasc'], FILTER_SANITIZE_STRING);
 $datavalidada=explode('-',$datanasc);
@@ -22,30 +27,30 @@ $month=$datavalidada[1];
 $day=$datavalidada[2];
 
 if(checkdate($month,$day,$year)==true) {
-  echo $datanasc . "<br>";  
+  echo "Data de nascimento: (yyyy-mm-dd) " . $datanasc . "<br>";  
 } else {
-    echo "Foi informada uma data invalida!<br>";
+    header('Location: cadastro.html');
 }
 
 
 
 $endereco=@filter_var($_REQUEST['endereco'], FILTER_SANITIZE_STRING);
-echo $endereco . "<br>";
+echo "Endereço: " . $endereco . "<br>";
 
 $bairro=@filter_var($_REQUEST['bairro'], FILTER_SANITIZE_STRING);
-echo $bairro . "<br>";
+echo "Bairro: " . $bairro . "<br>";
 
 $estado=@filter_var($_REQUEST['estado'], FILTER_SANITIZE_STRING);
-echo $estado . "<br>";
+echo "Estado: " . $estado . "<br>";
 
 $cidade=@filter_var($_REQUEST['cidade'], FILTER_SANITIZE_STRING);
-echo $cidade . "<br>";
+echo "Cidade: " . $cidade . "<br>";
 
 $cep=@filter_var($_REQUEST['cep'], FILTER_SANITIZE_STRING);
-echo $cep . "<br>";
+echo "CEP: " . $cep . "<br>";
 
 $receber=@filter_var($_REQUEST['receber'], FILTER_SANITIZE_STRING);
-echo $receber . "<br>";	
+echo "Receber e-mails: " . $receber . "<br>";	
 
 
 ?>
