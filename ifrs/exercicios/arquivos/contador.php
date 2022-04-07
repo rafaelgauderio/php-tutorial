d73209f04f33ef6e3056b7f32bfda364f8adb90b
Rafael de Luca <deluca1712@gmail.com>
2022-04-07 12:42:47 -0300

    Correcao arquivo contador

--- ifrs/exercicios/arquivos/contador.php
+++ ifrs/exercicios/arquivos/contador.php
@@ -6,21 +6,27 @@
 
 <body>
 
-<h2>Contador de visitas</h2>
-<h4>Clique no contador para zerar as visitas e F5 para incrementar.</h4>
+	<h2>Contador de visitas</h2>
+	<h4>Clique no contador para zerar as visitas e F5 para incrementar.</h4>
 
-<?php
-if(file_exists("visitantes.txt")) {
-$arquivo=fopen("visitantes.txt","r");
+	<?php
+	if (!file_exists('visitantes.txt')) {
+		$contador=0;
+		$arquivo=fopen("visitantes.txt","wb");
+		fwrite($arquivo,$contador);
+		fclose($arquivo);	
+	} else {
+
+	$arquivo=fopen("visitantes.txt","r");
 	$contador=fread($arquivo,50); //arquivo com 50 bytes de tamanho
 	$contador++;
 	$arquivo=fopen("visitantes.txt","w");
 	fwrite($arquivo,$contador);
 	fclose($arquivo);
+
 }
+
 ?>
-
-
 
 <br><a href="zerar.php"><?="Número todal de visitas: ". $contador ?></a>
 
